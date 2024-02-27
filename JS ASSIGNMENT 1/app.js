7779a96e6ed33a24970cf40775fdf954f619092f
ZAYAN KHAN <145120648+zayankhan99@users.noreply.github.com>
2024-02-27 10:30:12 +0500

    Add files via upload

---
+++ JS ASSIGNMENT 1/app.js
@@ -0,0 +1,50 @@
+// chapter  1
+// task 1
+// alert("Welcome to our website")
+// task 2
+// alert("Error! Please enter A valid password")
+// task 3
+// alert ("Welcome to JS land..\n Happy coding!")
+// task 4
+
+//chapter  2
+// task 1
+// var username = "zayan" ;
+// task 2
+// var myname ="ZAYAN KHAN" ;
+// task 3
+// var message ="HELLO WORLD" ;
+// alert (message)
+// task 4
+// alert ("ZAYAN KHAN")
+// alert("17 YEARS OLD")
+// alert ("WEB AND APP DEVELOPER")
+// task 5
+// var pizza = "pizza \n pizz \n piz\n pi \n p\n" ;
+// alert (pizza)
+// task 6
+// var email = "My Email address; zk9596492@gmail.com"
+// alert (email)
+// task 7
+// var book = "A smarter way to learn Javascript" ;
+// alert (book)
+// task 8
+// var yeah = ("yah !I can write HTML content through JavaScript") ;
+// document.write (yeah)
+// chapter 3
+// task 1
+// var age = "I am 17 years old" ;
+// alert (age)
+// task 2
+// var visit = "you have visited this site 15 times" ;
+// alert (visit)
+// task 3
+// var birthyear = "my birth year is 1990" ;
+//task 4
+// var visitorname = " ZAYAN KHAN ";
+// var producttitle = " t_-shirt";
+// var quantity = "5";
+// var final = "zayan khan ordered 5 small t-shirts on xyz clothing store";
+// document.write(final) 
+
+alert ("ALL CODE IS COMMENTED")
